9be7a41362f53532dff7f8a1b2b650007e359e8e
patidragoi <pati.dragoi@gmail.com>
2021-03-11 12:36:37 +0200

    laborator3

--- Laborator2/src/ro/ase/cts/g1088/laborator/modele/Cont.java
+++ Laborator2/src/ro/ase/cts/g1088/laborator/modele/Cont.java
@@ -1,9 +1,12 @@
 package ro.ase.cts.g1088.laborator.modele;
 
+import ro.ase.cts.g1088.laborator.exceptii.ExceptieFonduriInsuficiente;
+import ro.ase.cts.g1088.laborator.exceptii.ExceptieTransferIlegal;
+
 public abstract class Cont {
 	public abstract double getBalanta();
 	public abstract void alimenteazaContul(Double valoare);
-	public abstract void extrage(Double valoare);
+	public abstract void extrage(Double valoare) throws ExceptieFonduriInsuficiente , ExceptieTransferIlegal;
 	public abstract void transfer(Cont destinatie, Double valoare);
 
 }
