d1aeaa80dd85873a857d474c130c37f22a3b779b
patidragoi <pati.dragoi@gmail.com>
2021-04-08 12:37:19 +0300

    Implementare Factory

---
+++ Laborator7-Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/CaracterDisnay.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+public class CaracterDisnay extends CaracterJoc {
+
+	@Override
+	public void seDeplaseaza() {
+		
+		
+	}
+	
+
+}
---
+++ Laborator7-Factory Method si Facade/src/ro/ase/csie/cts/g1088/dp/factory/method/CaracterMarvel4Kids.java
@@ -0,0 +1,15 @@
+package ro.ase.csie.cts.g1088.dp.factory.method;
+
+public class CaracterMarvel4Kids extends CaracterJoc {
+	
+	public CaracterMarvel4Kids(String nume,int putere) {
+		this.nume=nume;
+		this.putere=putere;
+	}
+
+	@Override
+	public void seDeplaseaza() {
+		
+	}
+
+}
