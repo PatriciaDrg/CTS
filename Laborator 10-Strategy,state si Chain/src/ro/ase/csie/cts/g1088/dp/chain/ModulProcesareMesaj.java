48b82d4d98d51dae11a427a35b07b6829217f758
patidragoi <pati.dragoi@gmail.com>
2021-05-13 12:25:06 +0300

    commit pauza

--- Laborator 10-Strategy,state si Chain/src/ro/ase/csie/cts/g1088/dp/chain/ModulProcesareMesaj.java
+++ Laborator 10-Strategy,state si Chain/src/ro/ase/csie/cts/g1088/dp/chain/ModulProcesareMesaj.java
@@ -4,5 +4,9 @@ public abstract class ModulProcesareMesaj {
 	
 	public ModulProcesareMesaj next=null;
 	public  abstract void procesareMesaj(MesajChat mesaj);
+	public void setNext(ModulProcesareMesaj next) {
+		this.next = next;
+	}
+	
 
 }
